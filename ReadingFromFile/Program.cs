886c80024fa54bb2cec069d9229d3b3ea3e93277
Melany Willis <melexiw@gmail.com>
2024-06-28 18:56:48 -0500

    Completed read from file challenge

---
+++ ReadingFromFile/Program.cs
@@ -0,0 +1,41 @@
+﻿try
+{
+    StreamReader sr = new StreamReader("/Users/mylaptop/Projects/py4e/romeo.txt");
+    string line = sr.ReadLine();
+    
+    while (line != null) 
+    {
+        char[] charactersInString = line.ToLower().ToCharArray();
+        Dictionary<char, int> characters = new Dictionary<char, int>();
+        foreach (char character in charactersInString) {
+            if(char.IsLetter(character)){
+                if(characters.ContainsKey(character))
+                {
+                characters[character] ++;
+                }
+                else 
+                {
+                characters[character] = 1;
+                }
+
+        }
+    
+}
+
+foreach (KeyValuePair<char, int> c in characters) {
+    Console.WriteLine(c.Key + " : " + c.Value );
+}
+        line = sr.ReadLine();
+    }
+    sr.Close();
+    Console.ReadLine();
+
+}
+catch(Exception e)
+{
+    Console.WriteLine("Exception: " + e.Message);
+}
+finally
+{
+    Console.WriteLine("Executing finally block");
+}
